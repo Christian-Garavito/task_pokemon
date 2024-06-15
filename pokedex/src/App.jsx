1c675bcb74c4c6b4a1fa306a64736fee051a6cb1
CobosFelipe <flipper7247@gmail.com>
2024-06-15 02:50:56 -0500

    Agregando componente de busqueda

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -18,8 +18,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <p>count is {count}</p>
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          +
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
