ba8b7372626e64278f733568aa1af043d6092320
Christian-Garavito <christian.garavito@cumplimos.co>
2024-06-14 21:23:56 -0500

    jefferson

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
- 
+ fetch("https://pokeapi.co/api/v2/pokemon")
+ .then((response)=>response.json())
+ .then((data)=> console.log(data))
+ .catch((error)=>console.log(error))
 
 
 
